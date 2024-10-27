3e6b31655d0a10be438adeac38bab2968400c710
Kevin Cristensen <ke.cristensen@duocuc.cl>
2024-10-27 20:50:50 -0300

    Progress Indicators finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -17,40 +17,10 @@ class MenuItem{
 
 const appMenuItems = <MenuItem>[
   MenuItem(
-    title: 'Home',
-    subTitle: 'Go to Home',
-    link: '/',
-    icon: Icons.home
-  ),
-  MenuItem(
     title: 'Cards',
     subTitle: 'Go to Cards',
     link: '/cards',
     icon: Icons.credit_card
-  ),
-  MenuItem(
-    title: 'About',
-    subTitle: 'Go to About',
-    link: '/about',
-    icon: Icons.info
-  ),
-  MenuItem(
-    title: 'Contact',
-    subTitle: 'Go to Contact',
-    link: '/contact',
-    icon: Icons.contact_mail
-  ),
-  MenuItem(
-    title: 'Settings',
-    subTitle: 'Go to Settings',
-    link: '/settings',
-    icon: Icons.settings
-  ),
-  MenuItem(
-    title: 'Help',
-    subTitle: 'Go to Help',
-    link: '/help',
-    icon: Icons.help
   ),
   MenuItem(
     title: 'Buttons',
@@ -59,9 +29,9 @@ const appMenuItems = <MenuItem>[
     icon: Icons.touch_app
   ),
   MenuItem(
-    title: 'Exit',
-    subTitle: 'Exit the app',
-    link: '/exit',
-    icon: Icons.exit_to_app
+    title: 'ProgressIndicators',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded
   ),
 ];
